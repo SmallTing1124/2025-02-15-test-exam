23b2611a1dbb44d57ade04a34e12180b5a32600d
SmallTing1124 <chentt1124@gmail.com>
2025-02-15 14:30:05 +0800

    提交作答

--- app.js
+++ app.js
@@ -1,29 +1,35 @@
 function App() {
+  const [users, setUsers] = React.useState([]);
+  React.useEffect(() => {
+    (async () => {
+      try {
+        const res = await axios.get(`https://randomuser.me/api/?results=10`);
+        setUsers(res.data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    })()
+
+  }, [])
   return (
+
     <div className="container mx-auto p-4">
-      <div className="row">
-        <div className="col-md-4">
-          <div className="bg-light p-3">
-            <img
-              src="https://randomuser.me/api/portraits/women/61.jpg"
-              alt="頭像"
-              className="img-fluid rounded-circle"
-            />
-            <h2 className="mb-0">Mona Heen</h2>
-            <p className="mb-0">mona.heen@example.com</p>
-          </div>
-        </div>
-        <div className="col-md-4">
-          <div className="bg-light p-3">
-            <img
-              src="https://randomuser.me/api/portraits/women/10.jpg"
-              alt="頭像"
-              className="img-fluid rounded-circle"
-            />
-            <h2 className="mb-0">Susan Craig</h2>
-            <p className="mb-0">susan.craig@example.com</p>
-          </div>
-        </div>
+      <div className="row g-3">
+        {users.map((user, index) => {
+          return (
+            <div className="col-md-4" key={index}>
+              <div className="bg-light p-3">
+                <img
+                  src={user.picture.medium}
+                  alt="頭像"
+                  className="img-fluid rounded-circle"
+                />
+                <h2 className="mb-0">{`${user.name.first} ${user.name.last}` }</h2>
+                <p className="mb-0">{user.email}</p>
+              </div>
+            </div>
+          )
+        })}
       </div>
     </div>
   );
